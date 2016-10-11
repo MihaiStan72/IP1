d235fe82f2b3cb63d3b6bbef6f54a6777bbac51d
Mihai Stan <stan.mihai72@gmail.com>
2016-10-11 18:58:14 +0300

    Solved first 3 tasks
    1 2 and 3 solved.
    Modified .gitignore

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
+#include "t1.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+
     return 0;
+}
+
+bool isPalindrom(unsigned long long number) {
+    unsigned long long numberCopy = number, palindrome = 0;
+    while (number) {
+        palindrome *= 10;
+        palindrome += number % 10;
+        number /= 10;
+    }
+    return palindrome == numberCopy;
+}
+
+unsigned char sumBinaryFigure(unsigned long long number) {
+    unsigned char sum = 0;
+    while (number) {
+        sum += number % 2;
+        number /=2;
+    }
+    return sum;
+}
+
+bool isLeapYear(unsigned short year) {
+    if (year % 4) {
+        return false;
+    }
+    if (year % 100) {
+        return true;
+    }
+    if (year % 400) {
+        return false;
+    }
+    return true;
 }
 
 
