4de2886a4662d42443be35093c28ef63552a0b0b
MihaiStan72 <stan.mihai72@gmail.com>
2016-10-23 21:07:03 +0300

    8,9

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "t1.h"
 #include <iostream>
 using namespace std;
-
+unsigned long int maxDiv;
 int main() {
+	vector vec;
+	vec.length = 5;
+	int valori[] = { 0, 1, 1, 2, 3 };
+	for (int i = 0; i < vec.length; i++) {
+		vec.values[i] = valori[i];
+	}
+	cout << areOrderedFibonnaci(vec);
+	cin.get();
     return 0;
 }
 
@@ -21,7 +29,7 @@ bool isPalindrom (unsigned long long number) {
 unsigned char sumBinaryFigure (unsigned long long number) {
     unsigned char sum = 0;
 
-    while (number) {
+    while (number > 0) {
         sum += number % 2;
         number /= 2;
     }
@@ -74,10 +82,10 @@ unsigned int fibonnaci (int index) {
     switch (index) {
         case 0:
             return fibOne;
-
+			break;
         case 1:
             return fibTwo;
-
+			break;
         default:
             for (counter = 2; counter <= index; counter++) {
                 fibThree = fibOne + fibTwo;
@@ -119,7 +127,8 @@ unsigned long perfectNumbers (unsigned int number) {
 }
 
 unsigned short primeDivisors (unsigned int left, unsigned int right) {
-    bool ciur[right + 1];
+	const int upperLimit = right;
+	bool ciur[150000];
 
     for (int index = 0; index <= right; index++) {
         ciur [index] = true;
@@ -132,11 +141,15 @@ unsigned short primeDivisors (unsigned int left, unsigned int right) {
     unsigned long int index, primeIndex;
     for (index = left; index <= right; index++) {
         primeDivisorCount = 0;
+        for (primeIndex = 2; primeIndex <= index / 2; primeIndex++) {
+            if (ciur[primeIndex] && (index % primeIndex == 0)) {
                 primeDivisorCount++;
             }
         }
         if (primeDivisorCount > maximum) {
             maximum = primeDivisorCount;
+			maxDiv = maximum;
+            maxCount = 1;
         } else {
             if (primeDivisorCount == maximum) {
                 maxCount++;
@@ -146,6 +159,41 @@ unsigned short primeDivisors (unsigned int left, unsigned int right) {
     return maxCount;
 }
 
+matrix primeTwins(unsigned int count, unsigned int lowerBound) {
+	unsigned int currentNumber = lowerBound % 2 ? lowerBound + 2 : lowerBound + 1, numberOfPairs = 0;
+	bool foundPair = false;
+	matrix result;
+	result.lines = count;
+	result.columns = 2;
+	unsigned int sum = currentNumber * 2 + 2, higherPrime = currentNumber + 2, index;
+	while (numberOfPairs < count) {
+		higherPrime = sum / 2 + 1;
+		foundPair = true;
+		for (index = 2; index < higherPrime / 2; index++) {
+			if (higherPrime % index == 0 || (higherPrime - 2) % index == 0) {
+				foundPair = false;
+				break;
+			}
+		}
+		if (foundPair) {
+			result.values[numberOfPairs][0] = higherPrime - 2;
+			result.values[numberOfPairs][1] = higherPrime;
+			numberOfPairs++;
+		}
+		sum += 4;
+	}
+	return result;
+}
 
-
-
+bool areOrderedFibonnaci(vector vec) {
+	if (vec.values[0] != 0) {
+		return false;
+	}
+	int sum = vec.values[1], index;
+	for (index = 1; index < vec.length; index++) {
+		if (fibonnaci(index) != vec.values[index]) {
+			return false;
+		}
+	}
+	return true;
+}
